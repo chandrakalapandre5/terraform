536fa2c1e32b98bab0e2fbcd029eacb044f503fc
chandrakalapandre5 <164968831+chandrakalapandre5@users.noreply.github.com>
2024-05-19 17:09:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+provider "aws" {
+  region = "ca-central-1"
+}
 
- tags = {
-   Name = var.name_tag,
- }
+
+resource "aws_instance" "terraform-instance-1" {
+  ami                    = "ami-0c4596ce1e7ae3e68"
+  instance_type          = "t2.micro"
+  key_name               = "web"
+
+  tags = {
+    Name  = "my-ec2-instance"
+    appid = "terraform"
+    env   = "dev"
+  }
+  availability_zone = "ca-central-1a"
 }
